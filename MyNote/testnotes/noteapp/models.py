0e8548d2e2940307d3fcdcaeddf85a03123181d7
Oleksandr Martyniuk <sasha.martynuk.1@gmail.com>
2023-10-03 21:15:27 +0300

    Update models.py

--- MyNote/testnotes/noteapp/models.py
+++ MyNote/testnotes/noteapp/models.py
@@ -26,3 +26,11 @@ class Note(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+    
+    
+class PublicNote(models.Model):
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.author
